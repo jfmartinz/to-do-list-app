b58aa4bb06428572b2f0c1eb31606beecd49294c
Joseph Martin <se.josephmartin@gmail.com>
2025-03-07 18:03:22 +0800

    init files

---
+++ App.js
@@ -0,0 +1,19 @@
+// import { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
